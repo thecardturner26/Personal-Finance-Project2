a1abbf6df83f8d7f3c22d693f852ff2ce4264557
Marco Arteaga <marcoarteaga26@gmail.com>
2019-12-03 10:36:08 -0600

    new hub page

--- controllers/bills.js
+++ controllers/bills.js
@@ -3,12 +3,31 @@ const User = require('../models/user');
 
 
 module.exports = {
+    create,
     new: newBill,
     index
 }
 
 
-function newBill(req,res){
+function create(req, res){
+    req.body.nowShowing = !!req.body.nowShowing;
+    req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
+    if (req.body.cast) req.body.cast = req.body.cast.split(',');
+    for (let key in req.body){
+        if (req.body[key] === '') delete req.body[key]
+    }
+
+
+    var bill = new Bill(req.body);
+    bill.save(function(err){
+        if (err) return res.render('bills/new');
+        console.log(bill);
+        res.redirect('/bills');
+
+    });
+}
+
+function newBill(req, res){
     res.render('bills/new')
 }
 
